300dff8d9aca9287c911f9aa81a602e846c8fd19
taewoocode <qkrxodn6035@gmail.com>
2023-10-16 23:49:58 +0900

    Operation

--- src/basicoperation/Main.java
+++ src/basicoperation/Main.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner( System.in );
-        int A = in.nextInt();
-        int B = in.nextInt();
+        Scanner sc = new Scanner( System.in );
+        double A = sc.nextInt();
+        double B = sc.nextInt();
 
         System.out.println(A-B);
-        in.close(); // inclose로 닫아주는 걸 적극권장.
+        sc.close(); //
     }
 }
+
+//close로 닫아주는 걸 적극권장한다.
