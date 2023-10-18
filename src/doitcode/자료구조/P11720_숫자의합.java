f5d76a02bb9d0a70ca50891b132572dbd8206337
taewoocode <qkrxodn6035@gmail.com>
2023-10-18 21:05:02 +0900

    연습

---
+++ src/doitcode/자료구조/P11720_숫자의합.java
@@ -0,0 +1,21 @@
+package doitcode;
+
+import java.util.Scanner;
+
+public class P11720_숫자의합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for(int i = 0; i<cNum.length; i++){
+            sum += cNum[i] - '0';
+
+        }
+        System.out.println(sum);
+
+
+
+    }
+}
---
+++ src/doitcode/자료구조/P1546_평균.java
@@ -0,0 +1,2 @@
+package doitcode.자료구조;public class P1546_평균 {
+}
