f6707c49b523293dabd0619fd983dc6ab4da64e3
taewoocode <qkrxodn6035@gmail.com>
2024-03-27 17:36:54 +0900

    마라톤 문제 수정

--- src/programmers/Marathon.java
+++ src/programmers/Marathon.java
@@ -3,20 +3,21 @@ package programmers;
 import java.util.Arrays;
 
 public class Marathon {
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-
-        //1.두 배열을 정렬한다.
+    public String solution(String[] participant, String[] completion){
+        //1.배열을 정렬하기
         Arrays.sort( participant );
         Arrays.sort( completion );
 
+        //2.두 배열을 돌아서 찾는다.
         int i = 0;
         for (; i < completion.length; i++) {
-            if (participant[i].equals( completion[i] )) break;
+            if (!participant[i].equals( completion[i] )) break;
+
         }
         return participant[i];
     }
 
+    //debug
     public static void main(String[] args) {
         String[] part = {"leo", "kiki", "eden"};
         String[] comp = {"eden", "kiki"};
