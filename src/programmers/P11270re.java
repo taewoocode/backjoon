047b6ca6ab04757abcd52178c3d0574dfbca7440
taewoocode <qkrxodn6035@gmail.com>
2024-04-07 18:37:51 +0900

    N개의 합 출력

---
+++ src/programmers/P11270re.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class P11270re {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner( System.in );
+        int N = scanner.nextInt();
+        String sNum = scanner.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < cNum.length; i++) {
+            sum += cNum[i] - '0';
+        }
+        System.out.println( "sum = " + sum );
+    }
+}
